2d010421597bae5133e4f025ac4b1cf101efe90b
¨UP240356¨ <up240356@alumnos.upa.edu.mx>
2026-03-06 18:09:44 -0600

    Show pokemon image

--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,37 @@
 import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import PokemonCard from "@/componets/PokemonCard";
+
 
 export default function Index() {
-  const [results, setResults] = useState<any[]>([]);
-
+  const [results, setResults] = useState<any[]>([]); //guardan los datos en el resuts
+  
+//se ejecuta al inicio 
   useEffect(() => {
     console.log("Entre en pantalla");
     getPokemons();
   }, []);
 
+  //mandan a llamar los datos de la url
   const getPokemons = async () => {
     const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
     const response = await fetch(URL, {
-      method: "GET",
+      method: "GET",//get solo obtiene datos
     });
 
+    //se hace la peticion a internet
     const data = await response.json();
     console.log(data);
-    setResults(data.results);
+    setResults(data.results); //guarda pokemon
   };
 
+  //.map() recorre todos los elementos del arreglo.
+  //key identifica cada elemento de la lista 
   return (
-    <View>
+    <ScrollView>
       {results.map((pokemon) => (
-        <Text key={pokemon.name}>{pokemon.name}</Text>
+        <PokemonCard key={pokemon.name} name={pokemon.name} url={pokemon.url} />
       ))}
-    </View>
+    </ScrollView>
   );
 }
